078032466243cb4f01bf471a7782e7c564f79201
DonYoon <don.namkung@gmail.com>
2023-06-12 23:30:22 +1000

    Updated 12 Jun 2023

---
+++ js/portfolio-loader.js
@@ -0,0 +1,19 @@
+
+function loadPortfolio(portfolioId, fileName) {
+    fetch(fileName)
+      .then(response => response.text())
+      .then(html => {
+        document.getElementById(portfolioId).innerHTML = html;
+        // initialize Prism after loading the HTML
+        Prism.highlightAll();
+      });
+  }
+
+  loadPortfolio("portfolioModal1Container", "portfolios/portfolio1.html");
+  loadPortfolio("portfolioModal2Container", "portfolios/portfolio2.html");
+  loadPortfolio("portfolioModal3Container", "portfolios/portfolio3.html");
+  loadPortfolio("portfolioModal4Container", "portfolios/portfolio4.html");
+  loadPortfolio("portfolioModal5Container", "portfolios/portfolio5.html");
+  loadPortfolio("portfolioModal6Container", "portfolios/portfolio6.html");
+  loadPortfolio("portfolioModal7Container", "portfolios/portfolio7.html");
+  // Add more calls for other portfolios
