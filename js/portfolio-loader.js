12eda77bb2ced7df512bd41a512437e95a71c7db
DonYoon <don.namkung@gmail.com>
2023-06-16 13:28:33 +1000

    updated portfolio 16 Jun 2023

--- js/portfolio-loader.js
+++ js/portfolio-loader.js
@@ -1,20 +1,40 @@
+//js/portfolio-loader.js
 
 function loadPortfolio(portfolioId, fileName) {
-    fetch(fileName)
-      .then(response => response.text())
-      .then(html => {
-        document.getElementById(portfolioId).innerHTML = html;
-        // initialize Prism after loading the HTML
-        Prism.highlightAll();
+  fetch(fileName)
+    .then(response => response.text())
+    .then(html => {
+      document.getElementById(portfolioId).innerHTML = html;
+      // initialize Prism after loading the HTML
+      Prism.highlightAll();
+
+      // Show the modal after the HTML has been loaded
+      var modalId = portfolioId.replace('Container', '');
+      var modalElement = document.getElementById(modalId);
+      // Use setTimeout to delay the initialization until the HTML is rendered
+      setTimeout(() => {
+        var myModal = bootstrap.Modal.getInstance(modalElement);
+        // Only create a new Modal instance if it doesn't already exist
+        if (!myModal) {
+          myModal = new bootstrap.Modal(modalElement, {});
+        }
+        myModal.show();
+      }, 0);
+    });
+}
+
+document.addEventListener('DOMContentLoaded', (event) => {
+  const portfolioItems = document.getElementsByClassName('portfolio-item');
+
+  for(let i = 0; i < portfolioItems.length; i++) {
+      portfolioItems[i].addEventListener('click', function(e) {
+          e.preventDefault(); // Prevent the default click action
+          loadPortfolio(`portfolioModal${i+1}Container`, `portfolios/portfolio${i+1}.html`);
       });
   }
+});
 
-  loadPortfolio("portfolioModal1Container", "portfolios/portfolio1.html");
-  loadPortfolio("portfolioModal2Container", "portfolios/portfolio2.html");
-  loadPortfolio("portfolioModal3Container", "portfolios/portfolio3.html");
-  loadPortfolio("portfolioModal4Container", "portfolios/portfolio4.html");
-  loadPortfolio("portfolioModal5Container", "portfolios/portfolio5.html");
-  loadPortfolio("portfolioModal6Container", "portfolios/portfolio6.html");
-  loadPortfolio("portfolioModal7Container", "portfolios/portfolio7.html");
-  loadPortfolio("portfolioModal8Container", "portfolios/portfolio8.html");
-  // Add more calls for other portfolios
+
+
+ 
+
